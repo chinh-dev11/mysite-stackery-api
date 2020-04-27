45b9458ab80f4cc17cc415bd612e83a136b4e569
Chinh Le <chinh.le11@gmail.com>
2020-04-27 09:58:51 -0400

    Add dynamodb and response libs

--- src/getApps/index.js
+++ src/getApps/index.js
@@ -1,6 +1,29 @@
+const dynamodbLib = require('dynamodb-lib');
+const responseLib = require('response-lib');
+
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: '#Sect = :sect',
+    ExpressionAttributeNames: {
+      '#Sect': 'Section'
+    },
+    ExpressionAttributeValues: {
+      ':sect': {'S':'apps'}
+    },
+    ScanIndexForward: false
+  };
+
+  try {
+    const data = await dynamodbLib.call('query', params);
+    return responseLib.success(data.Items);
+  } catch (e) {
+    return responseLib.failure(e.statusCode, {
+      code: e.code,
+      message: e.message
+    });
+  }
 };
